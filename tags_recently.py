72a3da1f64aa189acca7324fbb29b1400ce6af87
ibotdotout <tkroputa@gmail.com>
2014-06-23 18:25:16 +0700

    update tags_recently to request api and get json

--- tags_recently.py
+++ tags_recently.py
@@ -2,7 +2,12 @@ def get_api():
     import config
     tag = config.tags[0]
     client_id = config.client_id
-    api = "http://api.instagram.com/v1/tags/{tag} \
-                /media/recent?client_id={client_id}"
+    api = "https://api.instagram.com/v1/tags/{tag}"\
+          "/media/recent?client_id={client_id}"
     api = api.format(tag=tag, client_id=client_id)
     return api
+
+
+def request_api_json():
+    import requests
+    return requests.get(get_api()).json()
--- tests/tags_recently_test.py
+++ tests/tags_recently_test.py
@@ -3,6 +3,11 @@ import tags_recently
 
 
 class TagsRecentlyTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.json = tags_recently.request_api_json()
+
     def test_has_config_file(self):
         import os.path
         fname = "config.py"
@@ -18,8 +23,16 @@ class TagsRecentlyTest(unittest.TestCase):
         self.assertTrue(config.tags,
                         "put your tags id in config file")
 
-    def test_correct_api(self):
+    def test_can_get_api(self):
         import config
         tag = config.tags[0]
-        self.assertRegexpMatches(tags_recently.get_api(), config.client_id)
-        self.assertRegexpMatches(tags_recently.get_api(), tag)
+        api = tags_recently.get_api()
+        self.assertRegexpMatches(api, config.client_id)
+        self.assertRegexpMatches(api, tag)
+
+    def test_request_api_get_something(self):
+        self.assertTrue(isinstance(self.json, dict))
+
+    def test_request_not_get_error(self):
+        if self.json:
+            self.assertEqual(self.json['meta']['code'], 200)
