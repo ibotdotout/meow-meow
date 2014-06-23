e12b28d28acacf36a85a2800461fdfc75b19658b
ibotdotout <tkroputa@gmail.com>
2014-06-23 18:25:41 +0700

    add models.py and log

---
+++ models.py
@@ -0,0 +1,31 @@
+class User():
+    def __init__(self, json):
+        self.username = json['username']
+        self.profile_picture = json['profiel_picture']
+        self.fullname = json['full_name']
+
+
+class Image():
+    def __init__(self, json):
+        self.url = json['url']
+        self.width = json['width']
+        self.height = json['height']
+
+
+class ImageResulutions():
+    def __init__(self, json):
+        self.low = Image(json['low_resolution'])
+        self.thumbnail = Image(json['thumbnail'])
+        self.standard = Image(json['standard_resolution'])
+
+
+class Caption():
+    def __init__(self, json):
+        self.text = json['text']
+
+
+class InstragramData():
+    def __init__(self, json):
+        self.user = User(json['user'])
+        self.caption = Caption(json['caption'])
+        self.image = ImageResulutions(json['images'])
