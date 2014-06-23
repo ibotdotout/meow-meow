f09ea2ac6ac5860a32003c5acc2fb9dc8505e163
ibotdotout <tkroputa@gmail.com>
2014-06-24 05:51:46 +0700

    add template tests/models

---
+++ tests/models_test.py
@@ -0,0 +1,23 @@
+import unittest
+import models
+
+
+class ModelsTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_user_model(self):
+        pass
+
+    def test_image_model(self):
+        pass
+
+    def test_image_resolutions_model(self):
+        pass
+
+    def test_caption_model(self):
+        pass
+
+    def InstragramItem(self):
+        pass
