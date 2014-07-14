2c92cb537a52d1a167ded36c9f6847f4b70aa209
ibotdotout <tkroputa@gmail.com>
2014-07-15 04:48:41 +0700

    update main

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-import api
-
-
 def get_items(json):
     import mapping
     return mapping.mapping_instragram_items(json)
 
 
 def recently_tag(tags):
+    import api
     tags_api = api.get_tags_recently_api(tags)
-    print(tags_api)
     return api.request_api(tags_api)
 
 
+def write_html(items):
+    with open('index.html', 'w') as file:
+        file.write('<html>')
+        file.write('<body>')
+        for i in items:
+            img = "<img src={0}></img>".format(i.images.thumbnail.url)
+            file.write(img)
+        file.write('</body>')
+        file.write('</html>')
+
+
+def tags_from_config():
+    import config
+    return config.tags
+
+
 def run():
-    tags = 'cat'
+    tags = tags_from_config()
+    tags = tags[0]
     respone = recently_tag(tags)
-    items = get_items(respone.json)
-    for i in items:
-        print(i.user)
+    items = get_items(respone.json())
+    if items:
+        print("look the cats")
+        write_html(items)
+    else:
+        print("Can't get items")
+
+if __name__ == '__main__':
+    run()
