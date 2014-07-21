93eee2da59a9115d89c9738be99ecebd384ce41b
ibotdotout <tkroputa@gmail.com>
2014-07-21 21:38:50 +0700

    use configparser instead of directconfig.py and make config.py that dynamic load configurations from environment varibles or config.ini

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
 import os
 
-# Configuration that load variables for Environment Variables
-# you can make you own configuration from config.py.sample
-client_id = os.environ['CLIENT_ID']
-tags = os.environ['TAGS']
+# Configuration from Environment Variables if can't load config.ini
+# you can make you own configuration from config.ini.sample
+
+if os.path.isfile("config.ini"):
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    client_id = config.get('DEFAULT', 'client_id')
+    tags = [i.strip() for i in config.get('DEFAULT', 'tags').split(',')]
+else:
+    client_id = os.environ['CLIENT_ID']
+    tags = os.environ['TAGS']
+
+print(client_id, tags)
