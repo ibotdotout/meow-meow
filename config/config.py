6d32535a85e99c3629e85775f9dc9b82aff75caa
ibotdotout <tkroputa@gmail.com>
2014-07-24 07:29:46 +0700

    add filter_keyword and make config.py support it

--- config/config.py
+++ config/config.py
@@ -7,20 +7,22 @@ import os
 CONFIG_PATH = "config/config.ini"
 client_id = os.environ.get('CLIENT_ID')
 raw_tags = os.environ.get('TAGS')
+raw_filter_keywords = os.environ.get('FILTER_KEYWORDS')
 
 
-def remove_quote(tags):
+def remove_quote(x):
     """ remove single and double quotes """
-    return raw_tags.replace('"', '').replace("'", "")
+    return x.replace('"', '').replace("'", "")
 
 
 def config_from_config_dot_ini(CONFIG_PATH):
     """ load configuration from config.ini """
     import configparser
-    global client_id, raw_tags
+    global client_id, raw_tags, raw_filter_keywords
     config = configparser.ConfigParser()
     config.read(CONFIG_PATH)
     client_id = config.get('DEFAULT', 'client_id')
+    raw_filter_keywords = config.get('DEFAULT', 'filter_keywords')
     raw_tags = config.get('DEFAULT', 'tags')
 
 
@@ -32,3 +34,5 @@ if config_ini_exists(CONFIG_PATH):
     config_from_config_dot_ini(CONFIG_PATH)
 
 tags = [i.strip() for i in remove_quote(raw_tags).split(',')]
+filter_keywords = [i.strip() for i in remove_quote(raw_filter_keywords)
+                   .split(',')]
