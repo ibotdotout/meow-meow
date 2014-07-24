94e9e26e0a8b0fcfec99b9a0a4ce47b52d74ac9a
ibotdotout <tkroputa@gmail.com>
2014-07-24 07:30:18 +0700

    add filter feature and fix mock mapping bug

--- meow_meow/core/mapping.py
+++ meow_meow/core/mapping.py
@@ -1,10 +1,12 @@
 from meow_meow.core import models
+from config import config
 
 
-def json_dict_to_object(json_dict, key, target_object):
+def json_dict_to_object(json_dict, key, target_object, filters=lambda x: True):
     if key in json_dict:
         if type(json_dict[key]) is list:
-            items = [target_object(item) for item in json_dict[key]]
+            items = [target_object(item) for item in json_dict[key] if
+                     filters(target_object(item))]
             return items
         else:
             return target_object(json_dict[key])
@@ -12,8 +14,17 @@ def json_dict_to_object(json_dict, key, target_object):
         raise NameError("Don't have {0} in json_dict".format(key))
 
 
+def mapping_filters(item, keywords):
+    return all([i not in item.caption.text.lower() for i in keywords])
+
+
 def mapping_instagram_items(json):
     key = 'data'
     if key in json:
+        keywords = config.filter_keywords
         target_object = models.InstagramItem
-        return json_dict_to_object(json, key, target_object)
+        filters = lambda x: mapping_filters(x, keywords)
+        if keywords:
+            return json_dict_to_object(json, key, target_object, filters)
+        else:
+            return json_dict_to_object(json, key, target_object)
--- tests/unit/core/test_mapping.py
+++ tests/unit/core/test_mapping.py
@@ -14,11 +14,27 @@ class MappingTest(unittest.TestCase):
             cls.json = json.load(json_data)
             cls.key = 'data'
 
-    @mock.patch('meow_meow.core.models')
-    def test_mapping_instagram_items(self, mock_target_obj):
-        mock_target_obj.InstagramItem = lambda x: x
+    @mock.patch('meow_meow.core.models.InstagramItem')
+    @mock.patch('config.config.filter_keywords',
+                new_callable=mock.PropertyMock(return_value=[]))
+    def test_mapping_instagram_items(self, mock_keywords,
+                                     mock_ig_item):
+        mock_ig_item.side_effect = lambda x: x
         result = mapping.mapping_instagram_items(self.json)
-        self.assertTrue(len(result) > 0)
+        self.assertTrue(len(result) >= 0)
+
+    @mock.patch('meow_meow.core.models.InstagramItem')
+    @mock.patch('config.config.filter_keywords',
+                new_callable=mock.PropertyMock(return_value=['cat', 'kitty']))
+    @mock.patch('meow_meow.core.mapping.mapping_filters')
+    def test_mapping_instagram_items_with_filters(self, mock_filters,
+                                                  mock_keywords, mock_ig_item):
+        mock_ig_item.side_effect = lambda x: x
+        mock_filters.side_effect = \
+            lambda x, keywords: all([i not in x['caption']['text'].lower()
+                                     for i in keywords])
+        result = mapping.mapping_instagram_items(self.json)
+        self.assertEqual(len(result), 4)
 
     def test_json_dict_to_object_len_correct(self):
         expected_result = len(self.json[self.key])
@@ -41,3 +57,9 @@ class MappingTest(unittest.TestCase):
         json = {'meta': {'code': 200}}
         self.assertRaises(NameError, mapping.json_dict_to_object, json,
                           self.key, dict())
+
+    def test_josn_dict_to_object_can_filter_item(self):
+        expected = 0
+        filters = lambda x: False
+        result = mapping.json_dict_to_object(self.json, 'data', dict, filters)
+        self.assertEqual(len(result), expected)
